1c7817c1c14d2e9356198586b6f34f602ab88feb
ppanagiotisilv <ppanagiotisilv@gmail.com>
2025-06-14 23:14:49 +0000

    server sign-in and fetch user from database (not from auth)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroobook.netlify.app",
   prefetch: true,
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: netlify()
 });
